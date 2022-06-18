e662a63f89fa9184334f994f2daf21eae140fb8e
J Bigelow (Prosper) <jordan@jordanbigelow.com>
2022-06-18 00:07:07 -0600

    Eliminated Docker File Trying to run in a manner similar to PSF/Black.

--- auto_bump/main.py
+++ auto_bump/main.py
@@ -2,53 +2,71 @@
 Primary module
 """
 
-from pathlib import Path
 import os
+import sys
+from pathlib import Path
+from subprocess import run, PIPE, STDOUT
+
 
 import yaml
 
 from .utils import increment_patch, increment_major, increment_minor
 
-chart = os.environ.get("CHART_NAME")
-strategy = os.environ.get("BUMP_STRATEGY", "PATCH")
+ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
+ENV_PATH = ACTION_PATH / ".black-env"
+ENV_BIN = ENV_PATH / ("Scripts" if sys.platform == "win32" else "bin")
+CHART = os.environ.get("CHART_NAME")
+STRATEGY = os.environ.get("BUMP_STRATEGY", "PATCH")
+
+run([sys.executable, "-m", "venv", str(ENV_PATH)], check=True)
+pip_process = run(
+    [str(ENV_BIN / "python"), "-m", "pip", "install", "PyYAML"],
+    stdout=PIPE,
+    stderr=STDOUT,
+    encoding="utf-8"
+)
+
+if pip_process.returncode:
+    print(pip_process.stdout)
+    print("::error::Failed to install dependencies", flush=True)
+    sys.exit(pip_process.returncode)
 
 
-def main(chart_name=chart, bump_strategy=strategy):
+long_option = Path(f"./{CHART}/Chart.yaml").resolve()
+short_option = Path(f"./{CHART}/Chart.yml").resolve()
 
-    long_option = Path(f"./{chart_name}/Chart.yaml").resolve()
-    short_option = Path(f"./{chart_name}/Chart.yml").resolve()
+file_path = long_option if long_option.is_file() else short_option if short_option.is_file() else False
 
-    file_path = long_option if long_option.is_file() else short_option if short_option.is_file() else False
+if file_path:
 
-    if file_path:
+    file = open(file_path, "w+")
+    chart_file = yaml.safe_load(file)
 
-        file = open(file_path, "w+")
-        chart_file = yaml.safe_load(file)
+    if "version" in chart_file:
+        current_version = chart_file["version"]
+        current_version = [int(v) for v in current_version.split(".")]
 
-        if "version" in chart_file:
-            current_version = chart_file["version"]
-            current_version = [int(v) for v in current_version.split(".")]
+        if STRATEGY.upper() == "PATCH":
+            new_version = "".join("%s." % str(a) for a in increment_patch(current_version))
 
-            if bump_strategy.upper() == "PATCH":
-                new_version = "".join("%s." % str(a) for a in increment_patch(current_version))
+        elif STRATEGY.upper() == "MINOR":
+            new_version = "".join("%s." % str(a) for a in increment_minor(current_version))
 
-            elif bump_strategy.upper() == "MINOR":
-                new_version = "".join("%s." % str(a) for a in increment_minor(current_version))
-
-            elif bump_strategy.upper() == "MAJOR":
-                new_version = "".join("%s." % str(a) for a in increment_major(current_version))
-                print(new_version)
-
-            else:
-                raise ValueError("bump_strategy is not correctly set.")
-
-            chart_file["version"] = new_version
-
-            with open("modified.yml", "w") as out_stream:
-                yaml.dump(chart_file, out_stream, default_flow_style=False, sort_keys=False)
+        elif STRATEGY.upper() == "MAJOR":
+            new_version = "".join("%s." % str(a) for a in increment_major(current_version))
 
         else:
-            raise AttributeError("Helm Chart %s is not properly configured" % chart_name)
+            raise ValueError("bump_strategy is not correctly set.")
+
+        print(new_version)
+
+        chart_file["version"] = new_version
+
+        with open("modified.yml", "w") as out_stream:
+            yaml.dump(chart_file, out_stream, default_flow_style=False, sort_keys=False)
 
     else:
-        raise TypeError("Chart File can not be found in %s chart package." % chart_name)
+        raise AttributeError("Helm Chart %s is not properly configured" % CHART)
+
+else:
+    raise TypeError("Chart File can not be found in %s chart package." % CHART)
